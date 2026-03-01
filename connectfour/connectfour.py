d40b9cf6e221f217fd2e2649aa929b4743a410c2
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-03-01 18:31:26 +0100

    Rename variables in ConnectFour

--- connectfour/connectfour.py
+++ connectfour/connectfour.py
@@ -13,50 +13,61 @@ from cython import (  # type: ignore
 
 @cclass
 class ConnectFour:  # TODO generalize
-    rows = 6
-    cols = 7
-    order: uint[7]
-    bottom: ulonglong[7]
-    top: ulonglong[7]
-    table: ulonglong[8388593]
-    key: ulonglong
+    rows: uint = 6
+    cols: uint = 7
+    move_order: uint[7]  # type: ignore
+    bottom_cells: ulonglong[7]  # type: ignore
+    top_cells: ulonglong[7]  # type: ignore
+    bottom_row: ulonglong
+    board: ulonglong
+    transpos_table: ulonglong[8388593]  # type: ignore
 
     def __cinit__(self) -> None:
+        one: ulonglong = 1
         col: uint
-        tmp: ulonglong
+        bottom_cell: ulonglong
+        top_cell: ulonglong
 
         if compiled:
-            self.order[0] = 3
-            self.order[1] = 2
-            self.order[2] = 4
-            self.order[3] = 1
-            self.order[4] = 5
-            self.order[5] = 0
-            self.order[6] = 6
-            tmp = 1
-            self.key = 0
+            self.move_order[0] = 3  # type: ignore
+            self.move_order[1] = 2  # type: ignore
+            self.move_order[2] = 4  # type: ignore
+            self.move_order[3] = 1  # type: ignore
+            self.move_order[4] = 5  # type: ignore
+            self.move_order[5] = 0  # type: ignore
+            self.move_order[6] = 6  # type: ignore
+            self.bottom_row = 0
+            self.board = (one << 49) - 1
             for col in range(7):
-                self.bottom[col] = tmp << (7 * col)
-                self.top[col] = tmp << (7 * col + 5)
-                self.key |= self.bottom[col]
+                bottom_cell = one << (7 * col)
+                top_cell = one << (7 * col + 5)
+                self.bottom_cells[col] = bottom_cell  # type: ignore
+                self.top_cells[col] = top_cell  # type: ignore
+                self.bottom_row |= bottom_cell
+                self.board ^= top_cell << 1
             for col in range(8388593):
-                self.table[col] = 0
+                self.transpos_table[col] = 0  # type: ignore
         else:
-            self.order = (3, 2, 4, 1, 5, 0, 6)
-            self.bottom = tuple(1 << (7 * col) for col in range(7))
-            self.top = tuple(1 << (7 * col + 5) for col in range(7))
-            self.table = [0] * 8388593
-            self.key = sum(self.bottom)
+            self.move_order = (3, 2, 4, 1, 5, 0, 6)  # type: ignore
+            self.bottom_cells = tuple(  # type: ignore
+                1 << (7 * col) for col in range(7)
+            )
+            self.top_cells = tuple(  # type: ignore
+                1 << (7 * col + 5) for col in range(7)
+            )
+            self.bottom_row = sum(self.bottom_cells)
+            self.board = ((1 << 49) - 1) ^ (sum(self.top_cells) << 1)
+            self.transpos_table = [0] * 8388593  # type: ignore
 
     @cfunc
     @inline
     def free(self, mask: ulonglong, col: uint) -> bint:
-        return mask & self.top[col] == 0
+        return mask & self.top_cells[col] == 0  # type: ignore
 
     @cfunc
     @inline
     def move(self, mask: ulonglong, col: uint) -> ulonglong:
-        return (mask + self.bottom[col]) | mask
+        return (mask + self.bottom_cells[col]) | mask  # type: ignore
 
     @cfunc
     @inline
@@ -86,22 +97,23 @@ class ConnectFour:  # TODO generalize
         alpha: cint,
         beta: cint,
     ) -> cint:
+        one: ulonglong = 1
         key: ulonglong
-        idx: cint
+        idx: uint
+        entry: ulonglong
         value: cint
         flag: uint
         col: uint
         new_mask: ulonglong
         new_position: ulonglong
         alpha_: cint
-        tmp: ulonglong
 
-        key = (self.key + mask) | position
+        key = (self.bottom_row + mask) | position
         idx = key % 8388593
-        tmp = 1
-        if self.table[idx] & ((tmp << 49) - 1) == key:
-            value = ((self.table[idx] >> 49) & ((tmp << 8) - 1)) - 21
-            flag = self.table[idx] >> (49 + 8)
+        entry = self.transpos_table[idx]  # type: ignore
+        if entry & ((one << 49) - 1) == key:
+            value = ((entry >> 49) & ((one << 8) - 1)) - 21
+            flag = entry >> (49 + 8)
             if flag == 0:
                 return value
             elif flag == 1 and value >= alpha:
@@ -112,17 +124,18 @@ class ConnectFour:  # TODO generalize
                 return value
         if depth == 0:
             return 0
-        for col in self.order:
+        for col in self.move_order:  # type: ignore
             if self.free(mask, col):
                 new_mask = self.move(mask, col)
                 if self.win(position | (new_mask ^ mask)):
                     value = (depth + 1) // 2
-                    tmp = value + 21
-                    self.table[idx] = key | (tmp << 49)
+                    entry = value + 21
+                    entry = key | (entry << 49)
+                    self.transpos_table[idx] = entry  # type: ignore
                     return value
         value = -21
         alpha_ = alpha
-        for col in self.order:
+        for col in self.move_order:  # type: ignore
             if self.free(mask, col):
                 new_mask = self.move(mask, col)
                 new_position = position ^ mask
@@ -141,10 +154,11 @@ class ConnectFour:  # TODO generalize
             flag = 1
         else:
             flag = 0
-        tmp = flag
-        tmp <<= 8
-        tmp |= value + 21
-        self.table[idx] = key | (tmp << 49)
+        entry = flag
+        entry <<= 8
+        entry |= value + 21
+        entry = key | (entry << 49)
+        self.transpos_table[idx] = entry  # type: ignore
         return value
 
     @ccall
