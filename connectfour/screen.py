ee674eb000ebad4092cc6a4a29b8b72375dee082
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-03-27 14:31:18 +0100

    Add screen.py

---
+++ connectfour/screen.py
@@ -0,0 +1,185 @@
+"""Classes for drawing."""
+
+__all__ = ["Displayable", "Screen"]
+
+from abc import ABC, abstractmethod
+from sys import platform, stdin
+from shutil import get_terminal_size
+from signal import signal, SIGWINCH
+from types import FrameType
+
+if platform == "win32":
+    from msvcrt import getch  # type: ignore
+
+    def get_stdin_attrs() -> list:
+        """Get the TTY attributes of the standard input."""
+        return []
+
+    def set_stdin_attrs(attrs: list) -> None:
+        """Set the TTY attributes of the standard input."""
+        pass
+
+    def set_stdin_raw() -> None:
+        """Set the mode of the standard input to raw."""
+        pass
+
+    def get_key() -> str:
+        """Read a keypress."""
+        key = getch()
+        if key in {b"\x00", b"\xe0"}:
+            key += getch()
+        return key.decode(errors="ignore")
+
+else:
+    from tty import setraw
+    from termios import tcgetattr, tcsetattr, TCSADRAIN
+
+    def get_stdin_attrs() -> list:
+        """Get the TTY attributes of the standard input."""
+        return tcgetattr(stdin)
+
+    def set_stdin_attrs(attrs: list) -> None:
+        """Set the TTY attributes of the standard input."""
+        tcsetattr(stdin, TCSADRAIN, attrs)
+
+    def set_stdin_raw() -> None:
+        """Set the mode of the standard input to raw."""
+        setraw(stdin)
+
+    def get_key() -> str:
+        """Read a keypress."""
+        key = stdin.read(1)
+        if key == "\x1b":
+            char = stdin.read(1)
+            key += char
+            if char in {"[", "O"}:
+                while True:
+                    char = stdin.read(1)
+                    key += char
+                    if char.isalpha() or char == "~":
+                        # if char == "M":
+                        #     key += stdin.buffer.read(3).decode(
+                        #         errors="ignore",
+                        #     )
+                        break
+        return key
+
+
+class Displayable(ABC):
+    """Displayable object."""
+
+    def __init__(self, screen: "Screen") -> None:
+        self._screen = screen
+        screen.add(self)
+
+    @abstractmethod
+    def display(self) -> None:
+        """Display the object."""
+
+    @abstractmethod
+    def handle_key(self, key: str) -> None:
+        """Handle input."""
+
+
+class Screen:
+    """Alternative screen buffer."""
+
+    @classmethod
+    def _print(cls, text: str) -> None:
+        """Print text."""
+        print(text, end="", flush=True)
+
+    def __init__(self) -> None:
+        self._cols = 0
+        self._rows = 0
+        self._buffer: list[str] = []
+        self._objects: list[Displayable] = []
+        self._focus: Displayable | None = None
+
+        def resize_handler(signum: int, frame: FrameType | None) -> None:
+            size = get_terminal_size()
+            cols = size.columns
+            rows = size.lines
+            self._cols = cols
+            self._rows = rows
+            self._buffer = [" "] * (cols * rows)
+            self.display()
+
+        self._stdin_attrs = get_stdin_attrs()
+        set_stdin_raw()
+        self._print("\x1b[?1049h\x1b[?25l\x1b[?1003h\x1b[?1006h")
+        resize_handler(int(SIGWINCH), None)
+        signal(SIGWINCH, resize_handler)
+
+    @property
+    def cols(self) -> int:
+        """Number of columns."""
+        return self._cols
+
+    @property
+    def rows(self) -> int:
+        """Number of rows."""
+        return self._rows
+
+    def _idx(self, row: int, col: int) -> int:
+        """Get the buffer index for a one-based position (row, column)."""
+        cols = self._cols
+        rows = self._rows
+        return (
+            col - 1 + (row - 1) * cols
+            if 1 <= col <= cols and 1 <= row <= rows
+            else -1
+        )
+
+    def __getitem__(self, key: tuple[int, int]) -> str:
+        idx = self._idx(key[0], key[1])
+        return self._buffer[idx] if idx >= 0 else ""
+
+    def __setitem__(self, key: tuple[int, int], value: str) -> None:
+        idx = self._idx(key[0], key[1])
+        if idx >= 0:
+            self._buffer[idx] = value
+
+    def add(self, obj: Displayable) -> None:
+        """Add a displayable object."""
+        self._objects.append(obj)
+        self.display()
+
+    def remove(self, obj: Displayable) -> None:
+        """Remove a displayable object."""
+        self._objects.remove(obj)
+        self.display()
+
+    def focus(self, obj: Displayable | None) -> None:
+        """Focus a displayable object."""
+        if obj in self._objects or obj is None:
+            self._focus = obj
+        self.display()
+
+    def clear(self) -> None:
+        """Clear the buffer."""
+        buffer = self._buffer
+        for i in range(len(buffer)):
+            buffer[i] = " "
+
+    def display(self) -> None:
+        """Display the buffer."""
+        self.clear()
+        for obj in self._objects:
+            obj.display()
+        self._print("\x1b[H" + "".join(self._buffer))
+
+    def listen_keys(self) -> None:
+        """Listen for input."""
+        while True:
+            key = get_key()
+            if key == "\x1b\x1b":
+                break
+            if self._focus is not None:
+                self._focus.handle_key(key)
+            self.display()
+
+    def close(self) -> None:
+        """Close the buffer."""
+        self._print("\x1b[?1049l\x1b[?25h\x1b[?1003l\x1b[?1006l")
+        set_stdin_attrs(self._stdin_attrs)
