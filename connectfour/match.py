a17f3128d11bf45f0409c5c46e061ec21c5fbd5b
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2026-03-29 01:40:44 +0100

    Improve readability in match.py

--- connectfour/match.py
+++ connectfour/match.py
@@ -59,13 +59,14 @@ class ConnectFourState:
         return self._game.free_col(self._occupied, i_col)
 
     def nearest_cols(self, i_col: int) -> Generator[int, None, None]:
+        n_cols = self._n_cols
         yield i_col
-        for i in range(1, self._n_cols):
+        for i in range(1, n_cols):
             next_i_col = i_col - i
             if next_i_col >= 0:
                 yield next_i_col
             next_i_col = i_col + i
-            if next_i_col < self._n_cols:
+            if next_i_col < n_cols:
                 yield next_i_col
 
     def winning_position(self) -> bool:
@@ -142,44 +143,52 @@ class ConnectFourMatch(Interactable):
         return f"\x1b[{30 + color}m\u25cf\x1b[0m"  # \u2b24
 
     def display(self) -> None:
-        row_offset = (self._screen.rows - self._n_rows + 1) // 2 + 1
-        col_offset = (self._screen.cols - 2 * self._n_cols) // 2 + 1
+        state = self._state
+        n_rows = state.n_rows
+        n_cols = state.n_cols
+        screen = self._screen
+        colors = self._colors
+        row_offset = (screen.rows - n_rows + 1) // 2 + 1
+        col_offset = (screen.cols - 2 * n_cols) // 2 + 1
         if not self._finished:
-            color = self._colors[self._state.turn]
-            self._screen[row_offset, self._move_col * 2 + col_offset] = (
+            color = colors[state.turn]
+            screen[row_offset, self._move_col * 2 + col_offset] = (
                 self._filled_cell(color)
             )
-        color1, color2 = self._colors
-        if self._state.turn == 1:
+        color1, color2 = colors
+        if state.turn == 1:
             color1, color2 = color2, color1
-        for i_row in range(self._n_rows):
-            for i_col in range(self._n_cols):
-                cell = 1 << (i_col * (self._n_rows + 1) + i_row)
-                if self._state.occupied & cell:
-                    color = color1 if self._state.position & cell else color2
+        for i_row in range(n_rows):
+            for i_col in range(n_cols):
+                cell = 1 << (i_col * (n_rows + 1) + i_row)
+                if state.occupied & cell:
+                    color = color1 if state.position & cell else color2
                     disc = self._filled_cell(color)
                 else:
                     disc = self._empty_cell()
-                self._screen[
-                    self._n_rows - i_row + row_offset, i_col * 2 + col_offset
-                ] = disc
+                screen[n_rows - i_row + row_offset, i_col * 2 + col_offset] = (
+                    disc
+                )
         for i, char in enumerate(self._status):
-            self._screen[self._n_rows + 2 + row_offset, i + col_offset] = char
+            screen[n_rows + 2 + row_offset, i + col_offset] = char
 
     def handle_event(self, event: tuple[str, ...]) -> None:
+        state = self._state
+        bots = self._bots
         move_str = ""
 
-        if event[0] == "key" and self._bots[self._state.turn] is None:
+        if event[0] == "key" and bots[state.turn] is None:
+            n_cols = state.n_cols
             not_finished = not self._finished
             key = event[1]
             if key == "\x1b[C" and not_finished:
-                for i_col in range(self._move_col + 1, self._n_cols):
-                    if self._state.free_col(i_col):
+                for i_col in range(self._move_col + 1, n_cols):
+                    if state.free_col(i_col):
                         self._move_col = i_col
                         break
             elif key == "\x1b[D" and not_finished:
                 for i_col in range(self._move_col - 1, -1, -1):
-                    if self._state.free_col(i_col):
+                    if state.free_col(i_col):
                         self._move_col = i_col
                         break
             elif (
@@ -187,43 +196,42 @@ class ConnectFourMatch(Interactable):
                 and not_finished
             ):
                 i_col = int(key) - 1
-                if i_col < self._n_cols and self._state.free_col(i_col):
+                if i_col < n_cols and state.free_col(i_col):
                     self._move_col = i_col
             elif key == "\r" and not_finished:
                 move_str = str(self._move_col + 1)
             elif key in {"\b", "\x7f"}:
-                self._state.pop()
-                if self._bots[self._state.turn] is not None:
-                    self._state.pop()
-                    bot = self._bots[self._state.turn]
+                state.pop()
+                if bots[state.turn] is not None:
+                    state.pop()
+                    bot = bots[state.turn]
                     if bot is not None:
                         bot.queue()
                 self._status = ""
                 self._finished = False
         elif event[0] == "state":
             string = event[1]
-            if string[:-1] == self._state.move_str:
+            if string[:-1] == state.move_str:
                 char = string[-1]
                 self._move_col = int(char) - 1
                 move_str = char
 
         if len(move_str) > 0:
-            self._state.push(move_str)
-            if self._state.winning_position():
+            state.push(move_str)
+            if state.winning_position():
                 self._status = (
-                    self._filled_cell(self._colors[self._state.prev_turn])
-                    + " Wins!"
+                    self._filled_cell(self._colors[state.prev_turn]) + " Wins!"
                 )
                 self._finished = True
             else:
-                for i_col in self._state.nearest_cols(self._move_col):
-                    if self._state.free_col(i_col):
+                for i_col in state.nearest_cols(self._move_col):
+                    if state.free_col(i_col):
                         self._move_col = i_col
                         break
                 else:
                     self._status = "Draw!"
                     self._finished = True
             if not self._finished:
-                bot = self._bots[self._state.turn]
+                bot = bots[state.turn]
                 if bot is not None:
                     bot.queue()
